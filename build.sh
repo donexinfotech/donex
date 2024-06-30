8ab4aafe78b0705b1d46202888490bc5e18fd732
Ankitsm38 <156781349+Ankitsm04@users.noreply.github.com>
2024-06-30 17:28:24 +0530

    push-2

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-cd Website
-npm install
-npm run build
+echo "Installing Python dependencies..."
+pip install -r requirements.txt
+echo "Running database migrations..."
+python manage.py migrate
+echo "Collecting static files..."
+python manage.py collectstatic
